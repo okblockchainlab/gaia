e4c22f40a00d848aa822184254ac52c146a62b08
banishee <gl12138@live.com>
2019-11-21 18:27:10 +0800

    save

--- cmd/flooder/scripts/sabi.sh
+++ cmd/flooder/scripts/sabi.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 for (( VAR = 0; VAR < 100; ++VAR )); do
+    echo "current:"${VAR} "start"
     w2="data/addr"${VAR}".txt"
     addrs=""
     for i in `cat ${w2}`
@@ -8,13 +9,14 @@ for (( VAR = 0; VAR < 100; ++VAR )); do
     addrs=${addrs}${i}","
     done
     s=${addrs%%","}
-    gaiacli tx send Turning ${s} 1000stake --fees 350stake -yes -b block --gas auto<<EOF
+    gaiacli tx send Turing ${s} 1000stake --fees 350stake -yes -b block --gas auto<<EOF
 12345678
 EOF
-    gaiacli tx staking delegate cosmosvaloper16wdljyzt5nvdjwrd06jup9hvc8wwhn5u6yl7w5 100stake ${s} --fees 1000stake -yes -b block --gas auto --from Turning<<EOF
+    sleep 1
+    gaiacli tx staking delegate cosmosvaloper16wdljyzt5nvdjwrd06jup9hvc8wwhn5u6yl7w5 100stake ${s} --fees 2000stake -yes -b block --gas auto --from Turing<<EOF
 12345678
 EOF
+    echo "current:"${VAR} "end"
     sleep 2
-    echo "current:"${VAR}
     #gaiacli query staking delegation cosmos14jl54akhlykp54nhe6gqppmevevxef7frgz0gx cosmosvaloper16wdljyzt5nvdjwrd06jup9hvc8wwhn5u6yl7w5
 done
