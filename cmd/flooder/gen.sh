ccda567da32e5d90313670f3fedc993a1c7f75d9
banishee <gl12138@live.com>
2019-11-21 18:01:00 +0800

    save

--- cmd/flooder/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for (( VAR = 0; VAR < 15; ++VAR )); do
-    w1="prikey"${VAR}".txt"
-    w2="addr"${VAR}".txt"
-    flooder account save_to_file --number 3000 --mnemonic_file_path ${w1} --address_file_path ${w2}
-done
-
